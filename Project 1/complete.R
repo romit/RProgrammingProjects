3e647b35117088bfa1af9641328d342165399e1e
romit <romitmitra@gmail.com>
2014-10-14 15:35:51 +0530

    Working initial commit

---
+++ Project 1/complete.R
@@ -0,0 +1,26 @@
+complete <- function(directory, id = 1:332){
+    
+    # Initialist a data frame which will be in the FOR loop
+    df <- data.frame()
+
+    for (i in id) {
+        
+        # Convert the numeric id into char filename with 3 characters along with ".csv"
+        filename <- sprintf("%03d.csv",i)
+        
+        # Construct path string using directory and filename
+        filepath <- paste(directory,filename,sep="/")
+        
+        # Read the file
+        d <- read.csv(filepath) 
+        
+        # Use rbind to build up a dataframe of passed ids
+        df <- rbind(df, c(i,nrow(d)))
+    }
+    
+    # At the end of the loop, print out the dataframe
+    names(df) <- c("id","nobs")
+    df 
+    
+    
+}
