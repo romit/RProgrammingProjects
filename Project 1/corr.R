62de32462f1422a8829d46bdcd0824a9987f4d3a
romit <romitmitra@gmail.com>
2014-10-15 14:15:02 +0530

    Restructured and simplified code -- not yet final
    Have restructured the code and simplified readability.
    
    Primary functionality is working ok.
    
    However fails for the following:
    cr <- corr("specdata", 5000)
    summary(cr)
    
    returns:
    Mode    NA's
    logical       0
    
    instead of expected:
    ##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
    ##

--- Project 1/corr.R
+++ Project 1/corr.R
@@ -1,50 +1,30 @@
 corr <- function(directory, threshold=0){
     
-    # Initialist a data frame which will be in the FOR loop
-    df <- data.frame()
-    id <- 1:length(list.files(directory))
+    # Run complete.R and get a dataframe of IDs and nobs
+    df <- complete(directory,1:332)
     
+    # Take subset of df with nobs are greater than threshold
+    dfsub <- subset(df, nobs > threshold)
     
-    for (i in id) {
-        
-        # Convert the numeric id into char filename with 3 characters along with ".csv"
-        filename <- sprintf("%03d.csv",i)
-        
-        # Construct path string using directory and filename
-        filepath <- paste(directory,filename,sep="/")
-        
-        # Read the file
-        d <- read.csv(filepath) 
-        
-        # Use rbind to build up a dataframe of passed ids
-        df <- rbind(df, c(i,nrow(d)))
-    }
-    
-    # At the end of the loop, print out the dataframe
-    names(df) <- c("id","nobs")
-
-    dfsub <- subset(df, df$nobs > threshold)
-    
+    # Initialise the vector to be constructed within the FOR loop
     corr <- vector()
     
-    
+    # Now the plan is to go over the files that have nobs greater than threshold
     for (i in dfsub$id){
         
         filename <- sprintf("%03d.csv", i)
         filepath <- paste(directory, filename, sep="/")
         
-        ## Load the data
-        d <- read.csv(filepath)
+        ## Load the data and omit NAs
+        dfcomplete <- na.omit(read.csv(filepath))
         
-        dfcomplete <- d[complete.cases(d),]
-        dcount <- nrow(dfcomplete)
-        
-        if(dcount >= threshold){
+        # Have already carried out the check by making a subset, not needed
+        # dcount <- nrow(dfcomplete)        
+        #if(dcount >= threshold){
             corr <- c(corr,cor(dfcomplete$nitrate, dfcomplete$sulfate))
             
-        }
+        #}
         
     }
-    corr <- corr[!is.na(corr)]
     corr
 }
